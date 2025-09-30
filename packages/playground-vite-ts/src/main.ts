6360aa4381222a0b697ba236703edac901ead673
curlykay <347936160@qq.com>
2025-09-30 15:50:13 +0800

    init

---
+++ packages/playground-vite-ts/src/main.ts
@@ -0,0 +1,17 @@
+import "./style.css";
+
+import { getLibRsLib } from "lib-rslib";
+import { getLibTsdown } from "lib-tsdown";
+import { getLibTsup } from "lib-tsup";
+
+const rootEl = document.querySelector("#app");
+if (rootEl) {
+  rootEl.innerHTML = `
+  <div class="content">
+
+    <p>${getLibRsLib()}</p>
+    <p>${getLibTsdown()}</p>
+    <p>${getLibTsup()}</p>
+  </div>
+`;
+}
